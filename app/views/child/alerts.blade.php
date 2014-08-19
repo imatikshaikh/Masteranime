490ae8464ddd4b0460e0596aeee2ff6600e0e534
Nex <nex.powerbot@hotmail.com>
2014-08-19 11:58:31 +0200

    Added Info alert

--- app/views/child/alerts.blade.php
+++ app/views/child/alerts.blade.php
@@ -1,11 +1,16 @@
 @if (!empty($msg_type) && $msg_type == 'success')
 <div class="alert alert-success alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Success!</strong> {{$msg or 'Unknown success'}}
+    <strong>Success!</strong> {{$msg or 'Unknown success ATLEAST IT WORKED!'}}
+</div>
+@elseif (!empty($msg_type) && $msg_type == 'info')
+<div class="alert alert-info alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>Info!</strong> {{$msg or 'Unknown info WUT?'}}
 </div>
 @else
 <div class="alert alert-danger alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Warning!</strong> {{$msg or 'Unknown warning'}}
+    <strong>Warning!</strong> {{$msg or 'Unknown warning WE SUCK'}}
 </div>
 @endif
