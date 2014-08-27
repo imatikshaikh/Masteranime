f3a08a97cab2f98735f5817f075a19502a5fb726
Nex <nex.powerbot@hotmail.com>
2014-08-27 17:25:19 +0200

    Anime chart display update

--- app/views/chart.blade.php
+++ app/views/chart.blade.php
@@ -20,54 +20,50 @@
                     </h3>
                 </div>
                 <div class="row-fluid">
-                    <div class="span12">
-                        <?php
-                        function getAnime()
-                        {
-                            $series = MasterAnime::getOngoingAnime();
-                            $anime = array();
-                            foreach ($series as $serie) {
-                                $latest = DB::table('latest_anime')->whereRaw('anime_id = ?', array($serie->id))->select('created_at', 'episode')->orderBy('created_at', 'DESC')->first();
-                                if (!empty($latest)) {
-                                    $today = new DateTime();
-                                    $future = new DateTime($latest->created_at);
-                                    $future->add(new DateInterval("PT168H"));
-                                    $interval = $future->diff($today);
-                                    if ($future > $today) {
-                                        $date = $interval->format('%d%H');
-                                        array_push($anime, array('episode' => $latest->episode, 'interval' => $interval, 'anime' => $serie, 'sort' => $date));
-                                    }
+                    <?php
+                    function getAnime()
+                    {
+                        $series = MasterAnime::getOngoingAnime();
+                        $anime = array();
+                        foreach ($series as $serie) {
+                            $latest = DB::table('latest_anime')->whereRaw('anime_id = ?', array($serie->id))->select('created_at', 'episode')->orderBy('created_at', 'DESC')->first();
+                            if (!empty($latest)) {
+                                $today = new DateTime();
+                                $future = new DateTime($latest->created_at);
+                                $future->add(new DateInterval("PT168H"));
+                                $interval = $future->diff($today);
+                                if ($future > $today) {
+                                    $date = $interval->format('%d%H');
+                                    array_push($anime, array('episode' => $latest->episode, 'interval' => $interval, 'anime' => $serie, 'sort' => $date));
                                 }
                             }
-                            return $anime;
                         }
+                        return $anime;
+                    }
 
-                        $series = getAnime();
-                        usort($series, function ($a1, $a2) {
-                            return ($a1["sort"] < $a2["sort"]) ? -1 : 1;
-                        });
-                        $count = 0;
-                        $total_count = 0;
-                        $total = count($series);
-                        foreach ($series as $serie) {
-                            if ($count == 0) {
-                                echo '<div class="row-fluid div-cover">';
-                            }
-                            echo '<div class="span2 clearfix"> <a data-toggle="tooltip-bottom" title="' . $serie["anime"]->name . ' - ep. ' . ((int)$serie["episode"] + 1) . '" href="' . URL::to('anime/' . $serie["anime"]->id . '/' . str_replace(array(" ", "/"), "_", $serie["anime"]->name)) . '" class="met_our_team_photo">' . HTML::image(Anime::getCover($serie["anime"]), $serie["anime"]->name . '_cover') . '</a><div class="met_our_team_name met_color clearfix" style="font-size: 12px;">';
-                            if ($serie["interval"]->d > 0) {
-                                echo $serie["interval"]->d . 'days ';
-                            }
-                            echo $serie["interval"]->h . 'hrs ' . $serie["interval"]->i . 'mins ';
-                            echo '</div></div>';
-                            $count++;
-                            $total_count++;
-                            if ($total == $total_count || $count >= 6) {
-                                $count = 0;
-                                echo '</div>';
-                            }
+                    $series = getAnime();
+                    usort($series, function ($a1, $a2) {
+                        return ($a1["sort"] < $a2["sort"]) ? -1 : 1;
+                    });
+                    $count = 0;
+                    $total_count = 0;
+                    $total = count($series);
+                    foreach ($series as $serie) {
+                        $time = '';
+                        if ($serie["interval"]->d > 0) {
+                            $time .= $serie["interval"]->d . 'days ';
                         }
-                        ?>
-                    </div>
+                        $time .= $serie["interval"]->h . 'hrs ' . $serie["interval"]->i . 'mins ';
+                        echo View::make('child.card_anime', array(
+                            "anime_id" => $serie["anime"]->id,
+                            "anime_name" => $serie["anime"]->name,
+                            "anime_episode" => ((int)$serie["episode"] + 1),
+                            "anime_img" => Anime::getCover($serie["anime"]),
+                            "time" => $time,
+                            "chart" => true
+                        ));
+                    }
+                    ?>
                 </div>
             </div>
         </div>
--- app/views/child/card_anime.blade.php
+++ app/views/child/card_anime.blade.php
@@ -1,7 +1,15 @@
 @if (!empty($anime_id) && !empty($anime_name) && !empty($anime_episode) && !empty($anime_img) && !empty($time))
+@if (empty($chart))
 <a href="{{ URL::to('/watch/anime/' . $anime_id . '/' . str_replace(array(" ", " / ", " ? "), '_', $anime_name) . '/' . $anime_episode) }}">
+@else
+<a href="{{ URL::to('/anime/' . $anime_id . '/' . str_replace(array(" ", " / ", " ? "), '_', $anime_name)) }}">
+@endif
 <div class="span3 anime_card" data-toggle="tooltip" title="{{ $anime_name }}">
+    @if (empty($chart))
     <div class="top_title">ep. {{ $anime_episode }} - {{ Latest::time_elapsed_string($time) }}</div>
+    @else
+    <div class="top_title">{{ $time }}</div>
+    @endif
     {{ HTML::image($anime_img, $anime_name) }}
     <p>
         @if (strlen($anime_name) > 23)
