8bb0800318582593a659541826a6eeb02f3ef9b2
Nex <nex.powerbot@hotmail.com>
2014-08-31 18:23:57 +0200

    routing tweak

--- app/views/child/card_anime.blade.php
+++ app/views/child/card_anime.blade.php
@@ -1,8 +1,8 @@
 @if (!empty($anime_id) && !empty($anime_name) && !empty($anime_episode) && !empty($anime_img) && !empty($time))
 @if (empty($chart))
-<a href="{{ URL::to('/watch/anime/' . $anime_id . '/' . str_replace(array(" ", " / ", " ? "), '_', $anime_name) . '/' . $anime_episode) }}">
+<a href="{{ URL::to('/watch/anime/' . $anime_id . '/' . str_replace(array(" ", "/", "?"), '_', $anime_name) . '/' . $anime_episode) }}">
 @else
-<a href="{{ URL::to('/anime/' . $anime_id . '/' . str_replace(array(" ", " / ", " ? "), '_', $anime_name)) }}">
+<a href="{{ URL::to('/anime/' . $anime_id . '/' . str_replace(array(" ", "/", "?"), '_', $anime_name)) }}">
 @endif
 <div class="span3 anime_card" data-toggle="tooltip" title="{{ $anime_name }}">
     @if (empty($chart))
@@ -10,7 +10,8 @@
     @else
     <div class="top_title">{{ $time }}</div>
     @endif
-    {{ HTML::image($anime_img, $anime_name) }}
+    <img src="{{$anime_img}}" alt="{{$anime_name}}">
+
     <p>
         @if (strlen($anime_name) > 23)
         {{ substr($anime_name, 0, 23) }}..
